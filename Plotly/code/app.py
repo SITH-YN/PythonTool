743e6ad6f840e70ed99909cfdce21752d55b1403
9921349-Z100 <yota.nakamura@aisin.co.jp>
2023-11-24 10:17:20 +0900

    グラフレイアウト変更

--- Plotly/code/app.py
+++ Plotly/code/app.py
@@ -27,86 +27,38 @@ df_member_info_weekly = pd.read_csv("../data/MEMBER_Info_Weekly.csv")
 df_smr_worktime = pd.read_csv("../data/SMR_WorkTime.csv")
 
 fig = make_subplots(rows=3, cols=2, subplot_titles=["SMR-XXXXX-00001", "SMR-XXXXX-00001", "SMR-XXXXX-00002", "SMR-XXXXX-00002", "SMR-XXXXX-00003", "SMR-XXXXX-00003"])
-# 横向きの棒グラフ
 fig.add_trace(
     go.Bar(
-        x=[120],
-        y=["Plan Work Time[h]"],
+        x=[120.00],
+        y=["Landing"],
         orientation="h",
-        name="Plan Work Time[h]",
+        text="120.00[h]",
+        name="Landing Work Time(SMR-XXXXX-00001)",
     ), row=1, col=1
 )
 fig.add_trace(
     go.Bar(
-        x=[80],
-        y=["Estimate Work Time[h]"],
+        x=[80.00],
+        y=["Estimate"],
         orientation="h",
-        name="Estimate Work Time[h]",
+        text="80.00h",
+        name="Estimate Work Time(SMR-XXXXX-00001)",
     ), row=1, col=1
 )
 fig.add_trace(
     go.Bar(
-        x=[160],
-        y=["Landing Work Time[h]"],
+        x=[160.00],
+        y=["Plan"],
         orientation="h",
-        name="Landing Work Time[h]",
+        text="160.00h",
+        name="Plan Work Time(SMR-XXXXX-00001)",
     ), row=1, col=1
 )
-fig.add_trace(
-    go.Bar(
-        x=[240],
-        y=["Plan Work Time[h]"],
-        orientation="h",
-        name="Plan Work Time[h]",
-    ), row=2, col=1
-)
-fig.add_trace(
-    go.Bar(
-        x=[120],
-        y=["Estimate Work Time[h]"],
-        orientation="h",
-        name="Estimate Work Time[h]",
-    ), row=2, col=1
-)
-fig.add_trace(
-    go.Bar(
-        x=[200],
-        y=["Landing Work Time[h]"],
-        orientation="h",
-        name="Landing Work Time[h]",
-    ), row=2, col=1
-)
-fig.add_trace(
-    go.Bar(
-        x=[80],
-        y=["Plan Work Time[h]"],
-        orientation="h",
-        name="Plan Work Time[h]",
-    ), row=3, col=1
-)
-fig.add_trace(
-    go.Bar(
-        x=[50],
-        y=["Estimate Work Time[h]"],
-        orientation="h",
-        name="Estimate Work Time[h]",
-    ), row=3, col=1
-)
-fig.add_trace(
-    go.Bar(
-        x=[130],
-        y=["Landing Work Time[h]"],
-        orientation="h",
-        name="Landing Work Time[h]",
-    ), row=3, col=1
-)
-
-# 横向きの折れ線グラフ
 fig.add_trace(
     go.Scatter(
         mode="lines+markers",
         x=["2023/11/1", "2023/11/2", "2023/11/3", "2023/11/4", "2023/11/5", "2023/11/6", "2023/11/7"],
-        y=[120, 100, 80, 60, 40, 20, 0],
+        y=[120.00, 100.00, 80.00, 60.00, 40.00, 20.00, 0.00],
         name="Base(SMR-XXXXX-00001)",
         line=dict(color="red", dash="dot"),
         marker=dict(symbol="circle"),
@@ -116,17 +68,44 @@ fig.add_trace(
     go.Scatter(
         mode="lines+markers",
         x=["2023/11/1", "2023/11/2", "2023/11/3", "2023/11/4", "2023/11/5", "2023/11/6", "2023/11/7"],
-        y=[120, 80, 40, 20, 0, 0, 0],
+        y=[120.00, 80.00, 40.00, 20.00, 0.00, 0.00, 0.00],
         name="Target(SMR-XXXXX-00001)",
         line=dict(color="blue", dash="solid"),
         marker=dict(symbol="circle"),
     ), row=1, col=2
 )
 fig.add_trace(
+    go.Bar(
+        x=[240.00],
+        y=["Landing"],
+        orientation="h",
+        text="240.00[h]",
+        name="Landing Work Time(SMR-XXXXX-00002)",
+    ), row=2, col=1
+)
+fig.add_trace(
+    go.Bar(
+        x=[120.00],
+        y=["Estimate"],
+        orientation="h",
+        text="120.00h",
+        name="Estimate Work Time(SMR-XXXXX-00002)",
+    ), row=2, col=1
+)
+fig.add_trace(
+    go.Bar(
+        x=[200.00],
+        y=["Plan"],
+        orientation="h",
+        text="200.00[h]",
+        name="Plan Work Time(SMR-XXXXX-00002)",
+    ), row=2, col=1
+)
+fig.add_trace(
     go.Scatter(
         mode="lines+markers",
         x=["2023/11/1", "2023/11/2", "2023/11/3", "2023/11/4", "2023/11/5", "2023/11/6", "2023/11/7"],
-        y=[160, 130, 100, 70, 40, 10, 0],
+        y=[160.00, 130.00, 100.00, 70.00, 40.00, 10.00, 0.00],
         name="Base(SMR-XXXXX-00002)",
         line=dict(color="red", dash="dot"),
         marker=dict(symbol="circle"),
@@ -136,17 +115,44 @@ fig.add_trace(
     go.Scatter(
         mode="lines+markers",
         x=["2023/11/1", "2023/11/2", "2023/11/3", "2023/11/4", "2023/11/5", "2023/11/6", "2023/11/7"],
-        y=[160, 150, 130, 100, 60, 10, 0],
+        y=[160.00, 150.00, 130.00, 100.00, 60.00, 10.00, 0.00],
         name="Target(SMR-XXXXX-00002)",
         line=dict(color="blue", dash="solid"),
         marker=dict(symbol="circle"),
     ), row=2, col=2
 )
 fig.add_trace(
+    go.Bar(
+        x=[80.00],
+        y=["Landing"],
+        orientation="h",
+        text="80.00[h]",
+        name="Landing Work Time(SMR-XXXXX-00003)",
+    ), row=3, col=1
+)
+fig.add_trace(
+    go.Bar(
+        x=[50.00],
+        y=["Estimate"],
+        orientation="h",
+        text="50.00[h]",
+        name="Estimate Work Time(SMR-XXXXX-00003)",
+    ), row=3, col=1
+)
+fig.add_trace(
+    go.Bar(
+        x=[130.00],
+        y=["Plan"],
+        orientation="h",
+        text="130.00[h]",
+        name="Plan Work Time(SMR-XXXXX-00003)",
+    ), row=3, col=1
+)
+fig.add_trace(
     go.Scatter(
         mode="lines+markers",
         x=["2023/11/1", "2023/11/2", "2023/11/3", "2023/11/4", "2023/11/5", "2023/11/6", "2023/11/7"],
-        y=[200, 190, 170, 140, 100, 50, 0],
+        y=[200.00, 190.00, 170.00, 140.00, 100.00, 50.00, 0.00],
         name="Base(SMR-XXXXX-00003)",
         line=dict(color="red", dash="dot"),
         marker=dict(symbol="circle"),
@@ -156,12 +162,13 @@ fig.add_trace(
     go.Scatter(
         mode="lines+markers",
         x=["2023/11/1", "2023/11/2", "2023/11/3", "2023/11/4", "2023/11/5", "2023/11/6", "2023/11/7"],
-        y=[200, 190, 180, 170, 130, 80, 40],
+        y=[200.00, 190.00, 180.00, 170.00, 130.00, 80.00, 40.00],
         name="Target(SMR-XXXXX-00003)",
         line=dict(color="blue", dash="solid"),
         marker=dict(symbol="circle"),
     ), row=3, col=2
 )
+# fig['layout'].update(legend=dict(traceorder='reversed'))
 
 app = dash.Dash(__name__)
 
@@ -236,99 +243,99 @@ def update_graphs(active_cell):
     return "Click the table"
 
 
-test_fig = make_subplots(rows=3, cols=1, subplot_titles=["SMR-XXXXX-00001", "SMR-XXXXX-00002", "SMR-XXXXX-00003"])
-# 横向きの棒グラフ
-test_fig.add_trace(
-    go.Bar(
-        x=[120],
-        y=["Plan Work Time[h]"],
-        orientation="h",
-        name="Plan Work Time[h]",
-    ), row=1, col=1
-)
-test_fig.add_trace(
-    go.Bar(
-        x=[80],
-        y=["Estimate Work Time[h]"],
-        orientation="h",
-        name="Estimate Work Time[h]",
-    ), row=1, col=1
-)
-test_fig.add_trace(
-    go.Bar(
-        x=[160],
-        y=["Landing Work Time[h]"],
-        orientation="h",
-        name="Landing Work Time[h]",
-    ), row=1, col=1
-)
-test_fig.add_trace(
-    go.Bar(
-        x=[240],
-        y=["Plan Work Time[h]"],
-        orientation="h",
-        name="Plan Work Time[h]",
-    ), row=2, col=1
-)
-test_fig.add_trace(
-    go.Bar(
-        x=[120],
-        y=["Estimate Work Time[h]"],
-        orientation="h",
-        name="Estimate Work Time[h]",
-    ), row=2, col=1
-)
-test_fig.add_trace(
-    go.Bar(
-        x=[200],
-        y=["Landing Work Time[h]"],
-        orientation="h",
-        name="Landing Work Time[h]",
-    ), row=2, col=1
-)
-test_fig.add_trace(
-    go.Bar(
-        x=[80],
-        y=["Plan Work Time[h]"],
-        orientation="h",
-        name="Plan Work Time[h]",
-    ), row=3, col=1
-)
-test_fig.add_trace(
-    go.Bar(
-        x=[50],
-        y=["Estimate Work Time[h]"],
-        orientation="h",
-        name="Estimate Work Time[h]",
-    ), row=3, col=1
-)
-test_fig.add_trace(
-    go.Bar(
-        x=[130],
-        y=["Landing Work Time[h]"],
-        orientation="h",
-        name="Landing Work Time[h]",
-    ), row=3, col=1
-)
-test_fig.show()
+# test_fig = make_subplots(rows=3, cols=1, subplot_titles=["SMR-XXXXX-00001", "SMR-XXXXX-00002", "SMR-XXXXX-00003"])
+# # 横向きの棒グラフ
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[120],
+#         y=["Plan Work Time[h]"],
+#         orientation="h",
+#         name="Plan Work Time[h]",
+#     ), row=1, col=1
+# )
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[80],
+#         y=["Estimate Work Time[h]"],
+#         orientation="h",
+#         name="Estimate Work Time[h]",
+#     ), row=1, col=1
+# )
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[160],
+#         y=["Landing Work Time[h]"],
+#         orientation="h",
+#         name="Landing Work Time[h]",
+#     ), row=1, col=1
+# )
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[240],
+#         y=["Plan Work Time[h]"],
+#         orientation="h",
+#         name="Plan Work Time[h]",
+#     ), row=2, col=1
+# )
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[120],
+#         y=["Estimate Work Time[h]"],
+#         orientation="h",
+#         name="Estimate Work Time[h]",
+#     ), row=2, col=1
+# )
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[200],
+#         y=["Landing Work Time[h]"],
+#         orientation="h",
+#         name="Landing Work Time[h]",
+#     ), row=2, col=1
+# )
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[80],
+#         y=["Plan Work Time[h]"],
+#         orientation="h",
+#         name="Plan Work Time[h]",
+#     ), row=3, col=1
+# )
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[50],
+#         y=["Estimate Work Time[h]"],
+#         orientation="h",
+#         name="Estimate Work Time[h]",
+#     ), row=3, col=1
+# )
+# test_fig.add_trace(
+#     go.Bar(
+#         x=[130],
+#         y=["Landing Work Time[h]"],
+#         orientation="h",
+#         name="Landing Work Time[h]",
+#     ), row=3, col=1
+# )
+# test_fig.show()
 
-"""3.1.3 棒グラフ（Bar trace）"""
-# bar_trace1 = go.Bar(x=["120", "80", "240"], y=["Estimate"], orientation="h", name="Estimate")
-# bar_trace2 = go.Bar(x=["60", "20", "50"], y=["Remain"], orientation="h", name="Remain")
-# grouped_fig = go.Figure([bar_trace1, bar_trace2])
-# grouped_fig.show()
+# """3.1.3 棒グラフ（Bar trace）"""
+# # bar_trace1 = go.Bar(x=["120", "80", "240"], y=["Estimate"], orientation="h", name="Estimate")
+# # bar_trace2 = go.Bar(x=["60", "20", "50"], y=["Remain"], orientation="h", name="Remain")
+# # grouped_fig = go.Figure([bar_trace1, bar_trace2])
+# # grouped_fig.show()
 
-# test1_fig = make_subplots(rows=3, cols=1, subplot_titles=["SMR-XXXXX-00001", "SMR-XXXXX-00002", "SMR-XXXXX-00003"])
+# # test1_fig = make_subplots(rows=3, cols=1, subplot_titles=["SMR-XXXXX-00001", "SMR-XXXXX-00002", "SMR-XXXXX-00003"])
 
-bar_trace1 = go.Bar(y=["SMR-xxxx-00001", "SMR-xxxx-00002", "SMR-xxxx-00003"], x=[120, 40, 80], orientation="h", name="Estimate")
-bar_trace2 = go.Bar(y=["SMR-xxxx-00001", "SMR-xxxx-00002", "SMR-xxxx-00003"], x=[20, 80, 120], orientation="h", name="Remain")
-bar_trace3 = go.Bar(x=[160, 240, 320], y=["SMR-xxxx-00001", "SMR-xxxx-00002", "SMR-xxxx-00003"], orientation="h", name="Plan]", ),
-stacked_fig = go.Figure(
-    [bar_trace1, bar_trace2],
-    # 積み上げ棒グラフにするためのlayoutを設定
-    layout=go.Layout(barmode="stack"),
-)
-stacked_fig.show()
+# bar_trace1 = go.Bar(y=["SMR-xxxx-00001", "SMR-xxxx-00002", "SMR-xxxx-00003"], x=[120, 40, 80], orientation="h", name="Estimate")
+# bar_trace2 = go.Bar(y=["SMR-xxxx-00001", "SMR-xxxx-00002", "SMR-xxxx-00003"], x=[20, 80, 120], orientation="h", name="Remain")
+# bar_trace3 = go.Bar(x=[160, 240, 320], y=["SMR-xxxx-00001", "SMR-xxxx-00002", "SMR-xxxx-00003"], orientation="h", name="Plan]", ),
+# stacked_fig = go.Figure(
+#     [bar_trace1, bar_trace2],
+#     # 積み上げ棒グラフにするためのlayoutを設定
+#     layout=go.Layout(barmode="stack"),
+# )
+# stacked_fig.show()
 
 
 if __name__ == '__main__':
