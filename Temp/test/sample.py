5ce3e1ecccc4831229e62d38c4010d43137abbde
9921349-Z100 <yota.nakamura@aisin.co.jp>
2024-04-09 14:00:40 +0900

    Add pytest code

---
+++ Temp/test/sample.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""sample."""
+
+# Standard Library
+# import csv
+# import os
+# import shutil
+# import tkinter
+
+# 3rd Party Library
+
+# Global Variable
+
+# Global Constant Define
+
+
+def main():
+    """sample."""
+
+    # Refer Global Variable
+
+    # Local Variable
+    x = 0
+    y = 0
+    sum = 0
+
+    # Local Constant Define
+
+    x = 10
+    y = 20
+    sum = func_sum(x, y)
+    print(sum)
+
+
+def func_sum(x, y):
+    """sample."""
+
+    # Refer Global Variable
+
+    # Local Variable
+    sum = 0
+
+    # Local Constant Define
+
+    sum = x + y
+    return sum
+
+
+if __name__ == "__main__":
+    main()
---
+++ Temp/test/test_sample.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""sample."""
+
+# Standard Library
+# import csv
+# import os
+# import shutil
+# import tkinter
+
+# 3rd Party Library
+import pytest
+import sample
+
+# Global Variable
+
+# Global Constant Define
+
+
+@pytest.mark.parametrize(("x", "y", "expected"), [
+    (10, 20, 30),
+    (20, 40, 60),
+])
+def test_func_sum(x, y, expected):
+    assert func_sum(x, y) == expected
